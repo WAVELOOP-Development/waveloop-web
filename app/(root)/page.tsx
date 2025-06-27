b971b5782fd0dd514b9cfd29a1242982ece2c32c
Navindu-Ashen <navindu158@gmail.com>
2025-06-27 10:43:26 +0530

    feat(workflow-CD): implements deployment workflow for firebase hosting

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <>
+      <main className="flex h-screen flex-col items-center justify-center p-24 ">
+        <span className="flex flex-col justify-center items-center">
+          <p className="text-[100px] font-extrabold uppercase text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500">
+            WAVELOOP
+          </p>
+          <p
+            className="text-xl tracking-[20px] text-white"
+          >
+            waves of innovation
+          </p>
+        </span>
+      </main>
+    </>
+  );
+}
