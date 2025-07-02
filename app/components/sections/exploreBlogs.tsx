6d499c70d09b755c891508e103a5944e38c66dfd
Tharusha_Perera <134818242+TharushaPerera27070@users.noreply.github.com>
2025-07-02 10:43:26 +0530

    feat(explore_blogs_page): add TextButton component and update ExploreBlogs layout

--- app/components/sections/exploreBlogs.tsx
+++ app/components/sections/exploreBlogs.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
 import CardSwap, { Card } from "@/components/react-bits/CardSwap/CardSwap";
 import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
 
